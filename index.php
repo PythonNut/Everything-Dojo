cc4fa71a288cee792bfa537609d99f57c6d03a03
Dojo-inc <donggyunkim@gmail.com>
2014-05-29 21:39:56 -0700

    link stylesheet

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Everything Dojo - AoPS Blog Resource</title>
+<link href="stylesheet.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
