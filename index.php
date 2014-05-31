3c6c41ead6805909d501900c3ddc58c6d0ee10e8
Deeg-Kim <donggyunkim@gmail.com>
2014-05-30 21:27:35 -0700

    update index.php and stylesheet.css

--- index.php
+++ index.php
@@ -8,11 +8,18 @@
 
 <body>
 
-<div id="content-main">
-	<div class="index-item" id="index-database"></div>
-    <div class="index-item" id="index-themizer"></div>
-    <div class="index-item" id="index-tryit"></div>
-    <div class="index-item" id="index-discuss"></div>
+<div id="index-content">
+	<div id="index-content-inner">
+    	<div class="index-row">
+        	<div class="index-item" id="index-database"></div>
+        	<div class="index-item" id="index-themizer"></div>
+        </div>
+        <div class="clear-40"></div>
+        <div class="index-row">
+        	<div class="index-item" id="index-tryit"></div>
+        	<div class="index-item" id="index-discuss"></div>
+    	</div>
+    </div>
 </div>
 
 </body>
