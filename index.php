6e6595a369dc922828a5720fa7870d382c2ba890
Tungsten314 <tungsten314@gmail.com>
2014-06-01 13:22:45 -0400

    Fixed broken links in index.php, changed link contents from image to text.

--- index.php
+++ index.php
@@ -6,16 +6,48 @@ include('include/header.php');
 
 <?php include('include/login-bar.php'); ?>
 <div id="index-content">
-	<div id="index-content-inner">
-    	<div class="index-row">
-        	<a href="<?php echo URL_DATABASE; ?>" class="index-item"><div class="index-item" id="index-database"><img class="index-img" src="images/index-database.png" /></div></a>
-        	<a href="<?php echo URL_THEMIZER; ?>" class="index-item"><div class="index-item" id="index-themizer"><img class="index-img" src="images/index-themizer.png" /></div></a>
+    <div id="index-content-inner">
+        <div class="index-row">
+            <a href=<?php echo "\"" . URL_DATABASE . "\""; ?> class="index-item">
+                <div class="index-item" id="index-database">
+                    <div class="index-text">
+                        <span class="index-title">DATABASE</span>
+                        <br />
+                        <span class="index-text">A collection of AoPS Blog CSS themes for your perusal.</span>
+                    </div>
+                </div>
+            </a>
+            <a href=<?php echo "\"" . URL_THEMIZER . "\""; ?> class="index-item">
+                <div class="index-item" id="index-themizer">
+                    <div class="index-text">
+                        <span class="index-title">THEMIZER</span>
+                        <br />
+                        <span class="index-text">Want to customize a theme for yourself? Then themeizer is the tool for you!</span>
+                    </div>
+                </div>
+            </a>
         </div>
         <div class="clear-40"></div>
         <div class="index-row">
-        	<a href="<?php echo URL_TRYIT; ?>" class="index-item"><div class="index-item" id="index-tryit"><img class="index-img" src="images/index-tryit.png" /></div></a>
-        	<a href="<?php echo URL_DISCUSS; ?>" class="index-item"><div class="index-item" id="index-discuss"><img class="index-img" src="images/index-discuss.png" /></div></a>
-    	</div>
+            <a href=<?php echo "\"" . URL_TRYIT . "\""; ?> class="index-item">
+                <div class="index-item" id="index-tryit">
+                    <div class="index-text">
+                        <span class="index-title">TRY-IT</span>
+                        <br />
+                        <span class="index-text">Want to try out a theme before you actually use it on your blog? Give it a try with this tool!</span>
+                    </div>
+                </div>
+            </a>
+            <a href=<?php echo "\"" . URL_DISCUSS . "\""; ?> class="index-item">
+                <div class="index-item" id="index-discuss">
+                    <div class="index-text">
+                        <span class="index-title">DISCUSS</span>
+                        <br />
+                        <span class="index-text">Come join our community of fellow AoPS bloggers and stylists!</span>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 </div>
 
