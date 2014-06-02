9cf0a095bca8ee0d0670c04c8f02a094659c90e5
Tungsten314 <tungsten314@gmail.com>
2014-06-02 16:20:45 -0400

    Created footer.php; changed #index-footer to #footer, added body opening tag to header.php.

--- index.php
+++ index.php
@@ -2,60 +2,58 @@
 include('include/include.php');
 include('include/header.php'); 
 ?>
-<body>
-  <?php include('include/login-bar.php'); ?>
-  <div id="index-content">
-    <div id="index-content-inner">
-      <div class="index-row">
+<?php include('include/login-bar.php'); ?>
+<div id="index-content">
+  <div id="index-content-inner">
+    <div class="index-row">
 
-        <a href=<?php echo "\"" . URL_DATABASE . "\""; ?> class="index-item">
-          <div class="index-item" id="index-database">
-              <div class="index-text">
-                <h3 class="index-title">DATABASE</h3>
-                <p class="index-text">A collection of AoPS Blog CSS themes for your perusal.</p>
-              </div>
-          </div>
-        </a>
-
-        <a href=<?php echo "\"" . URL_THEMIZER . "\""; ?> class="index-item">
-          <div class="index-item" id="index-themizer">
-              <div class="index-text">
-                <h3 class="index-title">THEMIZER</h3>
-                <p class="index-text">Want to customize a theme for yourself? Then themizer is the tool for you!</p>
-              </div>
-          </div>
-        </a>
-        
-      </div>
-
-      <div class="clear-40"></div>
-
-      <div class="index-row">
-
-        <a href=<?php echo "\"" . URL_TRYIT . "\""; ?> class="index-item">
-          <div class="index-item" id="index-tryit">
+      <a href=<?php echo "\"" . URL_DATABASE . "\""; ?> class="index-item">
+        <div class="index-item" id="index-database">
             <div class="index-text">
-              <h3 class="index-title">TRY-IT</h3>
-              <p class="index-text">Want to try out a theme before you actually use it on your blog? Give it a try with this tool!</p>
+              <h3 class="index-title">DATABASE</h3>
+              <p class="index-text">A collection of AoPS Blog CSS themes for your perusal.</p>
             </div>
-          </div>
-        </a>
+        </div>
+      </a>
 
-        <a href=<?php echo "\"" . URL_DISCUSS . "\""; ?> class="index-item">
-          <div class="index-item" id="index-discuss">
+      <a href=<?php echo "\"" . URL_THEMIZER . "\""; ?> class="index-item">
+        <div class="index-item" id="index-themizer">
             <div class="index-text">
-              <h3 class="index-title">DISCUSS</h3>
-              <p class="index-text">Come join our community of fellow AoPS bloggers and stylists!</p>
+              <h3 class="index-title">THEMIZER</h3>
+              <p class="index-text">Want to customize a theme for yourself? Then themizer is the tool for you!</p>
             </div>
-          </div>
-        </a>
+        </div>
+      </a>
+      
+    </div>
 
-      </div>
+    <div class="clear-40"></div>
+
+    <div class="index-row">
+
+      <a href=<?php echo "\"" . URL_TRYIT . "\""; ?> class="index-item">
+        <div class="index-item" id="index-tryit">
+          <div class="index-text">
+            <h3 class="index-title">TRY-IT</h3>
+            <p class="index-text">Want to try out a theme before you actually use it on your blog? Give it a try with this tool!</p>
+          </div>
+        </div>
+      </a>
+
+      <a href=<?php echo "\"" . URL_DISCUSS . "\""; ?> class="index-item">
+        <div class="index-item" id="index-discuss">
+          <div class="index-text">
+            <h3 class="index-title">DISCUSS</h3>
+            <p class="index-text">Come join our community of fellow AoPS bloggers and stylists!</p>
+          </div>
+        </div>
+      </a>
 
     </div>
+
   </div>
+</div>
 
-  <div id="index-footer"><?php echo COPYRIGHT; ?></div>
-
-</body>
-</html>
+<?php
+include('include/footer.php');
+?>
