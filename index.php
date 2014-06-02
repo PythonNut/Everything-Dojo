ce74c336a1530239b376647bc2280a1e22f13ee4
knittingfrenzy18 <knittingfrenzy18@gmail.com>
2014-06-02 14:48:17 -0400

    Because br's are bad. Because github style. Because whitespace :D

--- index.php
+++ index.php
@@ -3,51 +3,55 @@ include('include/include.php');
 include('include/header.php'); 
 ?>
 <body>
-
   <?php include('include/login-bar.php'); ?>
   <div id="index-content">
     <div id="index-content-inner">
       <div class="index-row">
+
         <a href=<?php echo "\"" . URL_DATABASE . "\""; ?> class="index-item">
           <div class="index-item" id="index-database">
               <div class="index-text">
-                <span class="index-title">DATABASE</span>
-                <br />
-                <span class="index-text">A collection of AoPS Blog CSS themes for your perusal.</span>
+                <h3 class="index-title">DATABASE</h3>
+                <p class="index-text">A collection of AoPS Blog CSS themes for your perusal.</p>
               </div>
           </div>
         </a>
+
         <a href=<?php echo "\"" . URL_THEMIZER . "\""; ?> class="index-item">
           <div class="index-item" id="index-themizer">
               <div class="index-text">
-                <span class="index-title">THEMIZER</span>
-                <br />
-                <span class="index-text">Want to customize a theme for yourself? Then themizer is the tool for you!</span>
+                <h3 class="index-title">THEMIZER</h3>
+                <p class="index-text">Want to customize a theme for yourself? Then themizer is the tool for you!</p>
               </div>
           </div>
         </a>
+        
       </div>
+
       <div class="clear-40"></div>
+
       <div class="index-row">
+
         <a href=<?php echo "\"" . URL_TRYIT . "\""; ?> class="index-item">
           <div class="index-item" id="index-tryit">
             <div class="index-text">
-              <span class="index-title">TRY-IT</span>
-              <br />
-              <span class="index-text">Want to try out a theme before you actually use it on your blog? Give it a try with this tool!</span>
+              <h3 class="index-title">TRY-IT</h3>
+              <p class="index-text">Want to try out a theme before you actually use it on your blog? Give it a try with this tool!</p>
             </div>
           </div>
         </a>
+
         <a href=<?php echo "\"" . URL_DISCUSS . "\""; ?> class="index-item">
           <div class="index-item" id="index-discuss">
             <div class="index-text">
-              <span class="index-title">DISCUSS</span>
-              <br />
-              <span class="index-text">Come join our community of fellow AoPS bloggers and stylists!</span>
+              <h3 class="index-title">DISCUSS</h3>
+              <p class="index-text">Come join our community of fellow AoPS bloggers and stylists!</p>
             </div>
           </div>
         </a>
+
       </div>
+
     </div>
   </div>
 
