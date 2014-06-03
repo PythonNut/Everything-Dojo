f590d2e8f2645399ef069e50aae2df5c3970ccf7
tyxchen <tyxchen@outlook.com>
2014-06-03 05:42:51 -0400

    deleted redundant html, added footer in tryit.php

--- tryit.php
+++ tryit.php
@@ -1,15 +1,11 @@
 <?php 
-include('include/include.php');
-include('include/header.php'); 
+  include('include/include.php');
+  include('include/header.php'); 
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>Try-It</title>
-</head>
 
-<body>
 <div id="tryit-header"></div>
-</body>
-</html>
+
+<?php
+  include('include/footer.php');
+?>
+
