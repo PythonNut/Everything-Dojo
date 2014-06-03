fc38fde4798bcf6a008d1177693f58731efdfc67
tyxchen <tyxchen@outlook.com>
2014-06-03 05:50:59 -0400

    added normalize.css, cleaned up HTML

--- include/header.php
+++ include/header.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8">
+
     <title>Everything Dojo - AoPS Blog Resource</title>
-    <link href="css/stylesheet.css" rel="stylesheet" />
+
+    <link href="css/normalize.css" rel="stylesheet">
+    <link href="css/stylesheet.css" rel="stylesheet">
+
     <script src="js/jquery.js"></script>
   </head>
 
