b378c8fb95c3ba06ac889df8e6ffb4c0d8350611
Deeg-Kim <donggyunkim@gmail.com>
2014-06-04 00:50:10 -0700

    fixed images
    I fixed the images/font issue. So the issue with the images on the index
    were the resizable thing. I resized all the images to be the same size
    so that when they are resized using vh, they will all have the same font
    size. This way, we can still have the crisp sharp text of the image but
    the resizabiltiy of the font.

--- index.php
+++ index.php
@@ -9,19 +9,17 @@
 
       <a href=<?php echo '"' . URL_DATABASE . '"'; ?> class="index-link">
         <div class="index-item" id="index-database">
-          <div class="index-text-container">
-            <h3 class="index-title">DATABASE</h3>
-            <p class="index-text">A collection of AoPS Blog CSS themes for your perusal.</p>
-          </div>
+        	<div class="index-text-container">
+				<img src="images/index-database.png" />
+            </div>
         </div>
       </a>
 
       <a href=<?php echo '"' . URL_THEMIZER . '"'; ?> class="index-link">
         <div class="index-item" id="index-themizer">
-          <div class="index-text-container">
-            <h3 class="index-title">THEMIZER</h3>
-            <p class="index-text">Want to customize a theme for yourself? Then themizer is the tool for you!</p>
-          </div>
+        	<div class="index-text-container">
+				<img src="images/index-themizer.png" />
+            </div>
         </div>
       </a>
       
@@ -33,19 +31,17 @@
 
       <a href=<?php echo '"' . URL_TRYIT . '"'; ?> class="index-link">
         <div class="index-item" id="index-tryit">
-          <div class="index-text-container">
-            <h3 class="index-title">TRY-IT</h3>
-            <p class="index-text">Want to try out a theme before you actually use it on your blog? Give it a try with this tool!</p>
-          </div>
+        	<div class="index-text-container">
+				<img src="images/index-tryit.png" />
+            </div>
         </div>
       </a>
 
       <a href=<?php echo '"' . URL_DISCUSS . '"'; ?> class="index-link">
         <div class="index-item" id="index-discuss">
-          <div class="index-text-container">
-            <h3 class="index-title">DISCUSS</h3>
-            <p class="index-text">Come join our community of fellow AoPS bloggers and stylists!</p>
-          </div>
+        	<div class="index-text-container">
+				<img src="images/index-discuss.png" />
+            </div>
         </div>
       </a>
 
