09fea141d74613561b0266bc16d76dfe78d8d48e
tyxchen <tyxchen@outlook.com>
2014-06-04 08:04:24 -0400

    reverted images to text

--- index.php
+++ index.php
@@ -3,23 +3,26 @@
   generateHeader('Index');
   include('include/login-bar.php');
 ?>
+
 <div id="index-content">
   <div id="index-content-inner">
     <div class="index-row">
 
       <a href=<?php echo '"' . URL_DATABASE . '"'; ?> class="index-link">
         <div class="index-item" id="index-database">
-        	<div class="index-item-container">
-				<img src="images/index-database.png" />
-            </div>
+          <div class="index-text-container">
+            <h3 class="index-title">DATABASE</h3>
+            <p class="index-text">A collection of AoPS Blog CSS themes for your perusal.</p>
+          </div>
         </div>
       </a>
 
       <a href=<?php echo '"' . URL_THEMIZER . '"'; ?> class="index-link">
         <div class="index-item" id="index-themizer">
-        	<div class="index-item-container">
-				<img src="images/index-themizer.png" />
-            </div>
+          <div class="index-text-container">
+            <h3 class="index-title">THEMIZER</h3>
+            <p class="index-text">Want to customize a theme for yourself? Then themizer is the tool for you!</p>
+          </div>
         </div>
       </a>
       
@@ -31,17 +34,19 @@
 
       <a href=<?php echo '"' . URL_TRYIT . '"'; ?> class="index-link">
         <div class="index-item" id="index-tryit">
-        	<div class="index-item-container">
-				<img src="images/index-tryit.png" />
-            </div>
+          <div class="index-text-container">
+            <h3 class="index-title">TRY-IT</h3>
+            <p class="index-text">Want to try out a theme before you actually use it on your blog? Give it a try with this tool!</p>
+          </div>
         </div>
       </a>
 
       <a href=<?php echo '"' . URL_DISCUSS . '"'; ?> class="index-link">
         <div class="index-item" id="index-discuss">
-        	<div class="index-item-container">
-				<img src="images/index-discuss.png" />
-            </div>
+          <div class="index-text-container">
+            <h3 class="index-title">DISCUSS</h3>
+            <p class="index-text">Come join our community of fellow AoPS bloggers and stylists!</p>
+          </div>
         </div>
       </a>
 
