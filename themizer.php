03ee72f1b3f98c8839471e6cf4196ef0d833c2e3
tyxchen <tyxchen@outlook.com>
2014-06-05 08:17:13 -0400

    added fun noscript to themizer

--- themizer.php
+++ themizer.php
@@ -3,6 +3,24 @@
   generateHeader('Themizer');
 ?>
 
+<noscript>
+  <div class="noscript background">
+    <div class="noscript wrapper">
+      <div class="noscript content">
+        <div class="noscript image">
+          <img src="/images/noscript.png">
+        </div>
+        <h3 class="noscript header">
+          Sorry, but Themizer requires JavaScript.
+        </h3>
+        <p class="noscript text">
+          Please enable it in your settings.
+        </p>
+      </div>
+    </div>
+  </div>
+</noscript>
+
 <div id="themizer-header"></div>
 
 <?php
