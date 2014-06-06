6b2756b486a9f16f03741eda8cf5db1b0b46fbfa
Tungsten314 <tungsten314@gmail.com>
2014-06-05 22:03:50 -0400

    Created unavailable page with slight humour and added it too all pages that aren't ready yet. Added Theme DB page. Fixed DB and themizer links in constants.php.

--- include/constants.php
+++ include/constants.php
@@ -1,9 +1,9 @@
 <?php
 define("COPYRIGHT", "&copy; 2014 Everything Dojo");
 
-define("URL_DATABASE", "http://rebuild.everythingdojo.com");
+define("URL_DATABASE", "http://rebuild.everythingdojo.com/database.php");
 define("URL_DISCUSS", "http://rebuild.everythingdojo.com");
-define("URL_THEMIZER", "http://rebuild.everythingdojo.com");
+define("URL_THEMIZER", "http://rebuild.everythingdojo.com/themizer.php");
 define("URL_TRYIT", "http://rebuild.everythingdojo.com/tryit.php");
 
 define("VERSION_DATABASE". "1.0");
