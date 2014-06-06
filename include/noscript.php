623732e0c4733e249848f3b9be28f7044c728b29
tyxchen <tyxchen@outlook.com>
2014-06-06 07:17:44 -0400

    clean up noscrip-related stuff

---
+++ include/noscript.php
@@ -0,0 +1,18 @@
+<noscript>
+  <div class="noscript background">
+    <div class="noscript wrapper">
+      <div class="noscript content">
+        <div class="noscript image content">
+          <img class="noscript image" src="images/noscript.png">
+        </div>
+        <h3 class="noscript header">
+          Sorry, but this page requires JavaScript.
+        </h3>
+        <p class="noscript text">
+          Please enable it in your settings.
+        </p>
+      </div>
+    </div>
+  </div>
+</noscript>
+
--- themizer.php
+++ themizer.php
@@ -3,27 +3,12 @@
   generateHeader('Themizer');
 ?>
 
-<noscript>
-  <div class="noscript background">
-    <div class="noscript wrapper">
-      <div class="noscript content">
-        <div class="noscript image">
-          <img src="images/noscript.png">
-        </div>
-        <h3 class="noscript header">
-          Sorry, but Themizer requires JavaScript.
-        </h3>
-        <p class="noscript text">
-          Please enable it in your settings.
-        </p>
-      </div>
-    </div>
-  </div>
-</noscript>
+<?php
+  include('include/unavailable.php');
+  include('include/noscript.php');
+?>
 
 <div id="themizer-header"></div>
-
-<?php include('include/unavailable.php'); ?>
 
 <?php
   include('include/footer.php');
