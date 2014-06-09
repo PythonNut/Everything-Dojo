7519e1ee1908073aa6f3325a4924c86fa683bf0b
Deeg-Kim <donggyunkim@gmail.com>
2014-06-08 22:24:53 -0700

    worked on tryit

--- tryit.php
+++ tryit.php
@@ -3,10 +3,9 @@
   generateHeader('Try-It');
 ?>
 
-<div id="tryit-header"></div>
-
-<?php include('include/unavailable.php'); ?>
-
+<div id="tryit-header"><div class="tryit-title">TRY-IT</div></div>
+<div id="tryit-blog">
+</div>
 <?php
   include('include/footer.php');
 ?>
