d4f282014cb6c0f3c4f5f5dae0ca28fa27ac57b3
Deeg-Kim <donggyunkim@gmail.com>
2014-06-08 22:27:37 -0700

    edit constants.php

--- include/constants.php
+++ include/constants.php
@@ -1,14 +1,16 @@
 <?php
-  define("COPYRIGHT", "&copy; 2014 Everything Dojo");
+  	include('creds.php');
 
-  define("URL_DATABASE", "http://rebuild.everythingdojo.com/database.php");
-  define("URL_DISCUSS", "http://rebuild.everythingdojo.com/discuss.php");
-  define("URL_THEMIZER", "http://rebuild.everythingdojo.com/themizer.php");
-  define("URL_TRYIT", "http://rebuild.everythingdojo.com/tryit.php");
+  	define("COPYRIGHT", "&copy; 2014 Everything Dojo");
 
-  define("VERSION_DATABASE". "1.0");
-  define("VERSION_DISCUSS". "1.0");
-  define("VERSION_THEMIZER". "1.0");
-  define("VERSION_TRYIT". "1.0");
+  	define("URL_DATABASE", "http://rebuild.everythingdojo.com/database.php");
+  	define("URL_DISCUSS", "http://rebuild.everythingdojo.com/discuss.php");
+  	define("URL_THEMIZER", "http://rebuild.everythingdojo.com/themizer.php");
+  	define("URL_TRYIT", "http://rebuild.everythingdojo.com/tryit.php");
+
+  	define("VERSION_DATABASE". "1.0");
+  	define("VERSION_DISCUSS". "1.0");
+  	define("VERSION_THEMIZER". "1.0");
+  	define("VERSION_TRYIT". "1.0");
 ?>
 
