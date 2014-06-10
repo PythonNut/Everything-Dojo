c2a772ba2ec04cd720869239e870a9ebe4b2cc59
Tungsten314 <tungsten314@gmail.com>
2014-06-10 09:30:51 -0400

    Created a constant with noscript content so that I can use it in get_header()

--- include/constants.php
+++ include/constants.php
@@ -12,5 +12,24 @@
   	define("VERSION_DISCUSS", "1.0");
   	define("VERSION_THEMIZER", "1.0");
   	define("VERSION_TRYIT", "1.0");
+
+    define("NOSCRIPT", '<noscript>
+<link rel="stylesheet" type="text/css" href="/css/noscript.css" />
+  <div class="noscript background">
+    <div class="noscript wrapper">
+      <div class="noscript content">
+        <div class="noscript image content">
+          <img class="noscript image" src="/images/noscript.png">
+        </div>
+        <h3 class="noscript header">
+          Sorry, but this page requires JavaScript.
+        </h3>
+        <p class="noscript text">
+          Please enable it in your settings.
+        </p>
+      </div>
+    </div>
+  </div>
+</noscript>');
 ?>
 
