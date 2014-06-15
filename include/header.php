042c996a5f491dfffae89d88abfeebfcb4775b98
tyxchen <tyxchen@outlook.com>
2014-06-15 16:54:41 -0400

    change logo path back to png as svg isn't ready yet

--- include/header.php
+++ include/header.php
@@ -57,7 +57,7 @@
           <?php } else { ?>
 
           <div id="logo">
-            <a href="/"><img src="/images/logo.svg"/></a>
+            <a href="/"><img src="/images/logo.png"/></a>
           </div>
 
           <nav>
