33f1273222abadeeb59d2452f684918ae38d9d67
Deeg-Kim <donggyunkim@gmail.com>
2014-06-16 19:17:40 -0700

    view body
    finished body basics, need to add pagination

--- include/themedb/view_body.php
+++ include/themedb/view_body.php
@@ -1,7 +1,7 @@
 <?php
 	$data = $themedb->get_themes();
 ?>
-<h2>Validated Themes</h2>
+<h2>Completed Themes</h2>
 
 <table class="database-table">
 	<thead>
@@ -17,7 +17,7 @@
 	for($i=0;$i<$data['validated']['count'];$i++){
 	?>
   	<tr>
-    	<td><?php echo $data['validated']['name'][$i]."<br />".$data['validated']['description'][$i]; ?></td>
+    	<td><?php echo "<b>".$data['validated']['name'][$i]."</b><br />".$data['validated']['description'][$i]; ?></td>
       <td><?php echo $data['validated']['author'][$i]; ?></td>
       <td><?php echo $data['validated']['version'][$i]; ?></td>
       <td><?php echo $data['validated']['stage'][$i]; ?></td>
@@ -28,4 +28,29 @@
   </tbody>
 </table>
 
-<h2>Unvalidated Themes</h2>
+<h2>Themes in Development</h2>
+
+<table class="database-table">
+	<thead>
+  	<tr>
+      <td>Theme</td>
+      <td>Author</td>
+      <td>Version</td>
+      <td>Stage</td>
+    <tr>
+  </thead>
+  <tbody>
+  <?php
+	for($i=0;$i<$data['unvalidated']['count'];$i++){
+	?>
+  	<tr>
+    	<td><?php echo "<b>".$data['unvalidated']['name'][$i]."</b><br />".$data['unvalidated']['description'][$i]; ?></td>
+      <td><?php echo $data['unvalidated']['author'][$i]; ?></td>
+      <td><?php echo $data['unvalidated']['version'][$i]; ?></td>
+      <td><?php echo $data['unvalidated']['stage'][$i]; ?></td>
+    </tr>
+  <?php
+	}
+	?>
+  </tbody>
+</table>
