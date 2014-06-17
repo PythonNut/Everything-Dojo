76be9a6e6f95212ed869fa125b52c859b382ad77
Deeg-Kim <donggyunkim@gmail.com>
2014-06-16 19:09:41 -0700

    database stuff
    working on view mode

--- include/themedb.php
+++ include/themedb.php
@@ -17,20 +17,86 @@ class themedb{
 	
 	function get_themes($id = 'all'){
 		if($id == 'all'){
-			// Select all themes
-			$query = "SELECT * FROM " . THEMEDB_TABLE;
+			// Select all approved themes but unvalidated
+			$query = "SELECT * FROM " . THEMEDB_TABLE . " WHERE `approved` = 1 AND `validated` = 0";
 			$result = mysql_query($query);
-
-			$data = mysql_fetch_array($result, MYSQL_ASSOC);
-
 			$num = mysql_num_rows($result);
+
+			$id 					= array();
+			$name 				= array();
+			$description 	= array();
+			$stage 				= array();
+			$author 			= array();
+			$version 			= array();
+			$submit_id 		= array();
+			$owner_id 		= array();
+			$i = 0;
 			// Assign themes to array
-			$name = array();			
-			$i=0;
 			while ($i < $num) {
-				$name[] = mysql_result($result,$i,"name");
+				$id[] 					= mysql_result($result,$i,"id");
+				$name[] 				= mysql_result($result,$i,"name");
+				$description[] 	= mysql_result($result,$i,"description");
+				$stage[] 				= mysql_result($result,$i,"stage");
+				$author[] 			= mysql_result($result,$i,"author");
+				$version[] 			= mysql_result($result,$i,"version");
+				$submit_id[] 		= mysql_result($result,$i,"submited_by_id");
+				$owner_id[] 		= mysql_result($result,$i,"owner_id");
 				$i++;
 			}			
+			$unvalidated = array(
+					'count'				=> $i,
+					'id' 					=> $id,
+					'name' 				=> $name,
+					'description' => $description,
+					'stage' 			=> $stage,
+					'author' 			=> $author,
+					'version' 		=> $version,
+					'submit_id' 	=> $submit_id,
+					'owner_id' 		=> $owner_id 				
+			);
+			
+			// Select all approved themes and validated
+			$query = "SELECT * FROM " . THEMEDB_TABLE . " WHERE `approved` = 1 AND `validated` = 1";
+			$result = mysql_query($query);
+			$num = mysql_num_rows($result);
+
+			$id 					= array();
+			$name 				= array();
+			$description 	= array();
+			$stage 				= array();
+			$author 			= array();
+			$version 			= array();
+			$submit_id 		= array();
+			$owner_id 		= array();
+			$i = 0;
+			// Assign themes to array
+			while ($i < $num) {
+				$id[] 					= mysql_result($result,$i,"id");
+				$name[] 				= mysql_result($result,$i,"name");
+				$description[] 	= mysql_result($result,$i,"description");
+				$stage[] 				= mysql_result($result,$i,"stage");
+				$author[] 			= mysql_result($result,$i,"author");
+				$version[] 			= mysql_result($result,$i,"version");
+				$submit_id[] 		= mysql_result($result,$i,"submited_by_id");
+				$owner_id[] 		= mysql_result($result,$i,"owner_id");
+				$i++;
+			}			
+			$validated = array(
+					'count'				=> $i,
+					'id' 					=> $id,
+					'name' 				=> $name,
+					'description' => $description,
+					'stage' 			=> $stage,
+					'author' 			=> $author,
+					'version' 		=> $version,
+					'submit_id' 	=> $submit_id,
+					'owner_id' 		=> $owner_id 				
+			);			
+			
+			$data = array(
+				'unvalidated' => $unvalidated,
+				'validated'		=> $validated
+			);			
 			
 			return $data;
 		}
--- include/themedb/view_body.php
+++ include/themedb/view_body.php
@@ -1,4 +1,32 @@
 <?php
 	$data = $themedb->get_themes();
-	print_r($data);
+	print_r($data['validated']);
 ?>
+<h2>Validated Themes</h2>
+
+<table class="database-table">
+	<thead>
+  	<tr>
+      <td>Theme</td>
+      <td>Author</td>
+      <td>Version</td>
+      <td>Stage</td>
+    <tr>
+  </thead>
+  <tbody>
+  <?php
+	for($i=0;$i<$data['validated']['count'];$i++){
+	?>
+  	<tr>
+    	<td><?php echo $data['validated']['name'][$i]."<br />".$data['validated']['description'][$i]; ?></td>
+      <td><?php echo $data['validated']['author'][$i]; ?></td>
+      <td><?php echo $data['validated']['version'][$i]; ?></td>
+      <td><?php echo $data['validated']['stage'][$i]; ?></td>
+    </tr>
+  <?php
+	}
+	?>
+  </tbody>
+</table>
+
+<h2>Unvalidated Themes</h2>
