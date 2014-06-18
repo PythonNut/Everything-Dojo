ce6a397c6425c77fc7bcc62213769142ca126534
knittingfrenzy18 <knittingfrenzy18@gmail.com>
2014-06-18 19:13:32 -0400

    A bunch of themizer stuff.
    half of it doesn't work/doesn't look good.
    Also some cleanup (constants change, fonts in a separate file)

---
+++ js/script.js
@@ -0,0 +1,100 @@
+//js/jQ functions that are available to run on any page. requires jQuery.
+
+//makes the header an absolutely positioned element
+//with a slide in/out button for pages which can't 
+//have elements affecting viewport size (themizer, try-it)
+function sliderHeader() {
+  var classcolor = $("header").attr("class");
+  var headerHeight = $("header").height();
+  $("header").css("position", "absolute")
+             .append("<div id=\"headerButton\" class=\"slideButton down " + classcolor + "\">Close</div>");
+  var openHeader = true;
+  $("#headerButton").click(function() {
+    $("#headerwrap").slideToggle();
+    if (openHeader) {
+      $("#headerButton").animate({
+        top: 0
+      }, 400, function() {
+        $(this).text("Open");
+      });
+      openHeader = false;
+    } else {
+      $(this).animate({
+        top: headerHeight
+      }, 400, function() {
+        $(this).text("Close");
+      });
+      openHeader = true;
+    }
+  });
+}
+
+function sliderSidebar() {
+  var color = $("#sidebar").css("background-color");
+  var sideWidth = $("#sidebar").width();
+  $("#sidebar").append("<div id=\"sideButton\" class=\"slideButton right\" style=\"background: " + color + "\">Close</div>");
+  var openSide = true;
+  $("#sideButton").click(function() {
+    $("#sidebar-inner").animate({width: 'toggle'}, 400);
+    if (openSide) {
+      $("#sideButton").animate({
+        left: 0
+      }, 400, function() {
+        $(this).text("Open");
+      });
+      openSide = false;
+    } else {
+      $(this).animate({
+        left: sideWidth
+      }, 400, function() {
+        $(this).text("Close");
+      });
+      openSide = true;
+    }
+  });
+}
+
+function themizer() {
+  //init
+  //unload style.css. Too much messing up stuff.
+  $("link[href='css/style.css']").remove();
+  //remove the header :D
+  $("header").remove();
+  //set index to be default mode
+  $("input[name='view'][value='index']").prop("checked", "checked");
+  $("#blog-body").load("blog-index.html");
+  
+  //view mode radios
+  $("input[name='view']").click(function() {
+    $("#blog-body").load("blog-" + $("input[name='view']:checked").val() + ".html");
+  });
+}
+
+/*laquo «
+&#187;) and it will looks like »*/
+
+//originally written by levans as a hide tag mod
+function post_hide(self) {
+  var e = $(self);
+  var d = e.next();
+  d.toggle();
+}
+
+//toggle_month for archives
+//from the blog software
+function toggle_month(month) {
+  thisMonth = document.getElementById('month_' + month);
+  thisImg = document.getElementById('month_image_' + month);
+
+  if (thisMonth && thisImg) {
+    if (thisMonth.style.display == "none") {
+      thisMonth.style.display = "block";
+      thisImg.src = "http://www.artofproblemsolving.com/Forum/blog/styles/hyperion/images/minus.gif";
+      thisImg.alt = '-';
+    } else {
+      thisMonth.style.display = "none";
+      thisImg.src = "http://www.artofproblemsolving.com/Forum/blog/styles/hyperion/images/plus.gif";
+      thisImg.alt = '+';
+    }
+  }
+}
--- themizer.php
+++ themizer.php
@@ -2,7 +2,24 @@
   $title = "Themizer";
   include("include/include.php");
   session_start();
+  $extra_style = "<link rel=\"stylesheet\" href=\"css/blog-style.css\" />
+  <link rel=\"stylesheet\" href=\"css/themizer.css\" />";
+  $extra_js = "<script>$(function(){
+    sliderSidebar('themizer-inner');
+    themizer();
+  });</script>";
   get_header();
 ?>
-
-<?php get_footer(); ?>
+<div id="sidebar">
+  <div id="sidebar-inner">
+    <div class="option" id="option-view">
+      <h5 class="option-title">Blog View</h5>
+      <p><input type="radio" name="view" value="index" /><label for="view" class="inline">Index</label></p>
+      <p><input type="radio" name="view" value="blog" /><label for="view" class="inline">Blog Post</label></p>
+      <p><input type="radio" name="view" value="post" /><label for="view" class="inline">Post New Entry</label></p>
+      <p><input type="radio" name="view" value="comment" /><label for="view" class="inline">Post New Comment</label></p>
+    </div>
+  </div>
+</div>
+<div id="blog-body"></div>
+<?php //no footer here ?>
