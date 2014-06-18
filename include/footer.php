4121b9888ee12a630384d209eaefc7f85fdda2a7
tyxchen <tyxchen@outlook.com>
2014-06-18 14:44:14 -0400

    New and improved sticky footer
    Based on http://ryanfait.com/html5-sticky-footer/
    
    NOTE: Since it's a sticky footer, it'll still
    push the body down without some hacks

--- include/footer.php
+++ include/footer.php
@@ -1,13 +1,15 @@
         <div class="clear"></div><?php //clear floating of main and side, for good measure ?>
       </div><?php //end #content ?>
 
-      <footer>
-        <p><a href="/">Home</a> &bull; <a href="/about.php">About</a></p>
-        <p>&copy;2014 Everything Dojo</p>
-        <p>Server Time: <?php echo date("m/d/y G:i T"); ?></p>
-      </footer>
+      <div class="push"></div>
 
     </div><?php //end wrap ?>
+
+    <footer>
+      <p><a href="/">Home</a> &bull; <a href="/about.php">About</a></p>
+      <p>&copy;2014 Everything Dojo</p>
+      <p>Server Time: <?php echo date("m/d/y G:i T"); ?></p>
+    </footer>
 
   </body>
 
