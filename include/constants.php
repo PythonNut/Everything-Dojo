8d836590558929f71c09acf0ff5ceb71514ae2ea
knittingfrenzy18 <knittingfrenzy18@gmail.com>
2014-06-18 19:13:32 -0400

    A bunch of themizer stuff.
    half of it doesn't work/doesn't look good.
    Also some cleanup (constants change, fonts in a separate file)

--- include/constants.php
+++ include/constants.php
@@ -1,14 +1,12 @@
 <?php
   /*constants*/
-	define("THEMEDB_TABLE", "theme_database");
-	
   define("URL_DATABASE", "database.php");
   define("URL_DISCUSS", "discuss.php");
   define("URL_THEMIZER", "themizer.php");
   define("URL_TRYIT", "tryit.php");
 
-  define("VERSION_DATABASE", "1.0");
-  define("VERSION_DISCUSS", "1.0");
-  define("VERSION_THEMIZER", "1.0");
-  define("VERSION_TRYIT", "1.0");
+  define("VERSION_DATABASE", "2.0");
+  define("VERSION_DISCUSS", "2.0");
+  define("VERSION_THEMIZER", "2.0");
+  define("VERSION_TRYIT", "2.0");
 ?>
