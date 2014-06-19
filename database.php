48c7d40d9489f773e0a461bc6823321c90e32fde
Deeg-Kim <donggyunkim@gmail.com>
2014-06-19 16:12:50 -0700

    whitespace issue

--- database.php
+++ database.php
@@ -5,7 +5,7 @@
   include("include/themedb.php");
   session_start();
   get_header();
-
+	
   if(isset($_GET['mode'])){
     $mode = $_GET['mode'];
   }
@@ -14,12 +14,16 @@
   }
 
   if(!isset($_SESSION['user_id'])){
+?>
+		<a href="index.php">Back Home</a>
+<?php		
 		include('include/themedb/view_body.php');
   // end guest case
   }
   else{
     switch($mode){
       case 'index':
+			
         include('include/themedb/index_body.php');
         break;
       case 'submit':
@@ -28,6 +32,11 @@
 <?php
         include('include/themedb/submit_body.php');
         break;
+			case 'request':
+?>		
+				<a href="<?php echo URL_DATABASE; ?>">Back to Database Index</a>	
+<?php			
+				break;	
       case 'view':
 ?>
 				<a href="<?php echo URL_DATABASE; ?>">Back to Database Index</a>
