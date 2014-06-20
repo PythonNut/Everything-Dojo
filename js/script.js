9922e61c131be712dcc335523dcac24c389a4111
tyxchen <tyxchen@outlook.com>
2014-06-19 21:45:11 -0400

    Major styling+jQ changes, code cleanup
    Changes:
    1) Fixed .collapsebutton icon bug
    2) aligned layout with mockup
    3) major jQ changes when sidebar slides in/out
    4) fixed conflicting ids
    5) clean up some code
    
    Notes:
    1) We don't need to preface selectors in themizer.css
       with #sidebar; as long as they're unique, we're OK
    2) we should probably use position: fixed on sidebar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-//js/jQ functions that are available to run on any page. requires jQuery.
+// js/jQ functions that are available to run on any page. requires jQuery.
 
-//makes the header an absolutely positioned element
-//with a slide in/out button for pages which can't 
-//have elements affecting viewport size (themizer, try-it)
+// makes the header an absolutely positioned element
+// with a slide in/out button for pages which can't
+// have elements affecting viewport size (themizer, try-it)
 function sliderHeader() {
   var classcolor = $("header").attr("class");
   var headerHeight = $("header").height();
@@ -29,41 +29,52 @@ function sliderHeader() {
   });
 }
 
+/**
+ * slide sidebar in/out
+ */
+
 function sliderSidebar() {
-  var color = $("#sidebar").css("background-color");
+  // figure out width of sidebar for positioning when hidden/shown
   var sideWidth = $("#sidebar").width();
-  $("#sidebar").append("<div id=\"sideButton\" class=\"slideButton right\" style=\"background: " + color + "\">Close</div>");
+  // add open/hide button
+  $("#sidebar").append("<div id=\"sideButton\" class=\"slideButton right\"></div>");
+  // sidebar is opened at first
   var openSide = true;
+  $("#sidebar").addClass("opened");
+  // main sliding function
   $("#sideButton").click(function() {
-    $("#sidebar-inner").animate({width: 'toggle'}, 400);
     if (openSide) {
+      $("#sidebar").removeClass("opened").addClass("closed").animate({
+        left: -sideWidth
+      }, 400);
       $("#sideButton").animate({
-        left: 0
-      }, 400, function() {
-        $(this).text("Open");
-      });
+        left: sideWidth
+      }, 400);
       openSide = false;
     } else {
+      $("#sidebar").removeClass("closed").addClass("opened").animate({
+        left: 0
+      }, 400);
       $(this).animate({
         left: sideWidth
-      }, 400, function() {
-        $(this).text("Close");
-      });
+      }, 400);
       openSide = true;
     }
   });
 }
 
 function themizer() {
-  //init
-  //unload style.css. Too much messing up stuff.
+  // init
+  // unload style.css. Too much messing up stuff.
   $("link[href='css/style.css']").remove();
-  //remove the header :D
+  // remove the header :D
   $("header").remove();
-  //set index to be default mode
+  // edit id of the wrapping #content to avoid conflict with #content in blog HTML
+  $("#content:first-of-type").attr("id", "themizer-content");
+  // set index to be default mode
   $("input[name='view'][value='index']").prop("checked", "checked");
   $("#blog-body").load("blog-index.html");
-  //option slides sliding init
+  // option slides sliding init
   $(".option").each(function() {
     var id = $(this).attr('id');
     $("#" + id + " .option-title").attr("onclick", "optionToggle('" + id + "')");
@@ -73,7 +84,7 @@ function themizer() {
   $("#" + firstop + " .option-title").removeClass("collapsed").addClass("expanded");
   $("#" + firstop + " .option-wrap").slideDown(0);
    
-  //view mode radios
+  // view mode radios
   $("input[name='view']").change(function() {
     $("#blog-body").load("blog-" + $("input[name='view']:checked").val() + ".html");
   });
@@ -91,15 +102,15 @@ function optionToggle(id) {
 /*laquo «
 &#187;) and it will looks like »*/
 
-//originally written by levans as a hide tag mod
+// originally written by levans as a hide tag mod
 function post_hide(self) {
   var e = $(self);
   var d = e.next();
   d.toggle();
 }
 
-//toggle_month for archives
-//from the blog software
+// toggle_month for archives
+// from the blog software
 function toggle_month(month) {
   thisMonth = document.getElementById('month_' + month);
   thisImg = document.getElementById('month_image_' + month);
