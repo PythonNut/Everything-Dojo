636883d703483a5011a24efc626ba0d20a9f84dc
tyxchen <tyxchen@outlook.com>
2014-06-21 19:01:23 -0400

    cleanup

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // js/jQ functions that are available to run on any page. requires jQuery.
 
-// makes the header an absolutely positioned element
-// with a slide in/out button for pages which can't
-// have elements affecting viewport size (themizer, try-it)
+/**
+ * makes the header an absolutely positioned element
+ * with a slide in/out button for pages which can't
+ * have elements affecting viewport size (themizer, try-it)
+ */
+
 function sliderHeader() {
   var classcolor = $("header").attr("class");
   var headerHeight = $("header").height();
@@ -36,11 +39,14 @@ function sliderHeader() {
 function sliderSidebar() {
   // figure out width of sidebar for positioning when hidden/shown
   var sideWidth = $("#sidebar").width();
+
   // add open/hide button
   $("#sidebar").append("<div id=\"sideButton\" class=\"slideButton right\"></div>");
+
   // sidebar is opened at first
   var openSide = true;
   $("#sidebar").addClass("opened");
+
   // main sliding function
   $("#sideButton").click(function() {
     if (openSide) {
@@ -49,17 +55,22 @@ function sliderSidebar() {
       }, 400, function() {
         $(this).removeClass("opened").addClass("closed");
       });
+
       $(this).animate({
         left: sideWidth
       }, 400);
+
       openSide = false;
+
     } else {
       $("#sidebar").removeClass("closed").addClass("opened").animate({
         left: 0
       }, 400);
+
       $(this).animate({
         left: sideWidth
       }, 400);
+
       openSide = true;
     }
   });
@@ -69,19 +80,25 @@ function themizer() {
   // init
   // unload style.css. Too much messing up stuff.
   $("link[href='css/style.css']").remove();
+
   // remove the header :D
   $("header").remove();
+
   // edit id of the wrapping #content to avoid conflict with #content in blog HTML
   $("#content:first-of-type").attr("id", "themizer-content");
+
   // set index to be default mode
   $("input[name='view'][value='index']").prop("checked", "checked");
   $("#blog-body").load("blog-index.html");
+
   // option slides sliding init
   $(".option").each(function() {
     var id = $(this).attr('id');
     $("#" + id + " .option-title").attr("onclick", "optionToggle('" + id + "')");
     optionToggle(id);
   });
+
+  // set first option to be open
   var firstop = $(".option:first-of-type").attr('id');
   $("#" + firstop + " .option-title").removeClass("collapsed").addClass("expanded");
   $("#" + firstop + " .option-wrap").slideDown(0);
@@ -91,15 +108,17 @@ function themizer() {
     $("#blog-body").load("blog-" + $("input[name='view']:checked").val() + ".html");
   });
 
-  // Side show/hide button stuff
-  // find width of sidebar and of button
+  // Show/hide sideButton
+  // find width of sidebar and of sideButton
   var sideWidth = $("#sidebar").width();
   var sideButtonWidth = $("#sideButton").width();
-  // hide #sideButton
+
+  // modify sideButton on click
   $("#sideButton").click(function() {
-    if ($("#sidebar").css("left") == "0px" && !$("#sidebar").hasClass("closed")) {
+    if ($("#sidebar").css("left") == "0px" && $("#sidebar").hasClass("opened")) { // fires when sidebar is to be closed
       $("#sideButton").addClass("triggered");
 
+      // timeout to remove triggered class and to hide sideButton
       setTimeout(function() {
         $("#sideButton").removeClass("triggered");
         $(".closed #sideButton").animate({
@@ -108,14 +127,16 @@ function themizer() {
       }, 4000);
     }
   });
+
+  // show sideButton on hover + scroll
   // taken and modified from http://css-tricks.com/snippets/jquery/fire-event-when-user-is-idle/
   var idleTimer = null;
   var idleState = false;
-  var idleWait = 4000;
 
   $(window).bind('mousemove scroll', function() {
-      clearTimeout(idleTimer);
+      clearTimeout(idleTimer); // clear timeout if user acts
 
+      // user active
       if (idleState == true) {
         // Reactivated event
         $(".closed #sideButton").addClass("triggered").animate({
@@ -125,21 +146,27 @@ function themizer() {
 
       idleState = false;
 
-      idleTimer = setTimeout(function () {
+      // user inactive
+      idleTimer = setTimeout(function() {
       // Idle Event
         $("#sideButton").removeClass("triggered");
         $(".closed #sideButton").animate({
           left: sideWidth - sideButtonWidth
         });
-      idleState = true; }, idleWait);
+        idleState = true;
+      }, 4000);
   });
 
-  $(window).trigger("mousemove");
+  $(window).mousemove();
 }
 
+/**
+ * Toggle options
+ */
 function optionToggle(id) {
   $("#" + id + " .option-wrap").slideToggle();
   var content = $("#" + id + " .option-title");
+
     if (content.hasClass("collapsed"))
       content.removeClass("collapsed").addClass("expanded");
     else
