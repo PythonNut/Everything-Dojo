5ad3d2cad25c02225a58931b2c142a9cdf1c212c
DG Kim <dg@DGs-MacBook-Pro.local>
2014-07-31 19:07:57 -0700

    submit step 1
    you can enter into the database now and is handled with the db handler

--- include/themedb/submit_body.php
+++ include/themedb/submit_body.php
@@ -1,6 +1,6 @@
 <h2>Submit a Theme</h2>
 
-<form method="post" action="">
+<form method="post" action="include/db-handler.php">
   <div class="col" id="col1">
     Theme name:<br />
     <input type="text" name="name" /><br />
@@ -10,7 +10,7 @@
   <div class="col" id="col2">
     Theme screenshot (url):<br />
     <input type="text" name="screenshot" /><br />
-    Theme version (i.e. 1.2):<br />
+    Theme version (e.g. 1.2):<br />
     <input type="text" name="version" />
   </div>
   <div class="col" id="col3">
@@ -25,5 +25,6 @@
     Theme CSS:<br />
     <textarea id="css" name="code"></textarea>
   	<input type="submit" name="submit" style="font-size: 15px;" />
+    <input type="hidden" name="mode" value="submit-theme" />
   </div>
 </form>
