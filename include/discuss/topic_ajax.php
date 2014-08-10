7d6cc747154b181db75772082573939f0426c2ae
encadyma@gmail.com <KMO@kdmo-ipad.local>
2014-08-10 14:29:23 -0700

    Fixes
    + ajax

--- include/discuss/topic_ajax.php
+++ include/discuss/topic_ajax.php
@@ -2,7 +2,10 @@
 session_start();
 switch($_POST['action']){
   case "thank":
-    $discuss->thank(intval($_POST['id']), intval($_POST['type']), intval($_SESSION['user_id']));
+    $result = $discuss->thank(intval($_POST['id']), intval($_POST['type']), intval($_SESSION['user_id']));
+    if ($result){
+      echo "";
+    }
   default:
     return "No action exists or defined. Try again.";
 }
