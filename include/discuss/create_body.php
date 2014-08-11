fca6560564b7a4b781c4d18c85ea1c6cb879636c
encadyma@gmail.com <encadyma@gmail.com>
2014-08-11 00:50:43 -0700

    PHP fix

--- include/discuss/create_body.php
+++ include/discuss/create_body.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 if (empty($_GET['c'])){
   echo "Can't create anything because you didn't tell me to do anything! Don't worry, though; Try going <a href='discuss.php'>back to Discuss home page</a> or try our other services!";
 }
@@ -10,30 +10,28 @@ else{
         break;
       }
       if (trim(strlen($_POST['title'])) < 5){
-        echo "Your title needs to have at least 5 characters (excluding spaces)!"; ?>
-        <fieldset id="topic-create-comment">
+        echo "Your title needs to have at least 5 characters (excluding spaces)!";
+        echo "<fieldset id=\"topic-create-comment\">
         <legend>Add new comment</legend>
-        <form action="<?php echo URL_DISCUSS?>?view=create&c=post&t=<?php echo intval($_GET['t']);?>" method="post">
-          Title: <input type="text" name="title" value="<?php echo $_POST['title']?>"/><br/>
-          Comment: <textarea name="desc" placeholder="Write your comment here..." style="vertical-align:top; height:200px;"><?php echo $_POST['desc']?></textarea><br/>
-          <input name="forum" value="<?php echo intval($_POST['forum']);?>" hidden="hidden"/>
-          <input type="submit" value="Comment"/>
+        <form action=".URL_DISCUSS."?view=create&c=post&t=".intval($_GET['t'])." method=\"post\">
+          Title: <input type=\"text\" name=\"title\" value=".$_POST['title']."/><br/>
+          Comment: <textarea name=\"desc\" placeholder=\"Write your comment here...\" style=\"vertical-align:top; height:200px;\">".$_POST['desc']."</textarea><br/>
+          <input name=\"forum\" value=".intval($_POST['forum'])." hidden=\"hidden\"/>
+          <input type=\"submit\" value=\"Comment\"/>
         </form>
-        </fieldset>
-  <?php 
+        </fieldset>";
       }
       else if (trim(strlen($_POST['desc'])) < 10){
-        echo "Your description needs to have at least 10 characters (excluding spaces)!"; ?>
-        <fieldset id="topic-create-comment">
+        echo "Your description needs to have at least 10 characters (excluding spaces)!"; 
+        echo "<fieldset id=\"topic-create-comment\">
         <legend>Add new comment</legend>
-        <form action="<?php echo URL_DISCUSS?>?view=create&c=post&t=<?php echo intval($_GET['t']);?>" method="post">
-          Title: <input type="text" name="title" value="<?php echo $_POST['title']?>"/><br/>
-          Comment: <textarea name="desc" placeholder="Write your comment here..." style="vertical-align:top; height:200px;"><?php echo $_POST['desc']?></textarea><br/>
-          <input name="forum" value="<?php echo intval($_POST['forum']);?>" hidden="hidden"/>
-          <input type="submit" value="Comment"/>
+        <form action=".URL_DISCUSS."?view=create&c=post&t=".intval($_GET['t'])." method=\"post\">
+          Title: <input type=\"text\" name=\"title\" value=".$_POST['title']."/><br/>
+          Comment: <textarea name=\"desc\" placeholder=\"Write your comment here...\" style=\"vertical-align:top; height:200px;\">".$_POST['desc']."</textarea><br/>
+          <input name=\"forum\" value=".intval($_POST['forum'])." hidden=\"hidden\"/>
+          <input type=\"submit\" value=\"Comment\"/>
         </form>
-        </fieldset>
-  <?php
+        </fieldset>";
       }
       else{ 
         if (intval($_POST['forum']) == 1){
@@ -55,7 +53,22 @@ else{
           }
         }
         else{
-          $query = "INSERT INTO `cyneerco_evdo`.`discuss_posts` (`post_id`, `user_id`, `topic_id`, `time`, `title`, `text`, `thanks`) VALUES (NULL, '7', '2', '100', 'wat', 'wat', '1|2|3|4|5|6|7|8');";
+          $query = "INSERT INTO ".DISCUSS_POSTS_TABLE." (user_id, topic_id, time, title, text) VALUES (:user, :topic, :time, :title, :text)";
+          $sth = $dbc->prepare($query);
+          $result = $sth->execute(array(
+            ':user' => intval($_SESSION['user_id']),
+            ':topic' => intval($_GET['t']),
+            ':time' => time(),
+            ':title' => htmlspecialchars($_POST['title']),
+            ':text' => htmlspecialchars($_POST['desc'])
+          ));
+          if ($result){
+            echo "Topic created! Redirecting...";
+            echo "<meta http-equiv=\"refresh\" content=\"1;URL='".URL_DISCUSS."?view=topic&f=".intval($_POST['forum'])."&t=".intval($_GET['t'])."'\" />";
+          }
+          else{
+            echo "Something wrong happened while posting your comment! Don't worry, though; Try going <a href='discuss.php'>back to Discuss home page</a> or try our other services!";
+          }
         }
       }
       break;
