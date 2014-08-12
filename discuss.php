03fcc38cebb7b31305a7a9d23b7adc153b7304dd
DG Kim <donggyunkim@gmail.com>
2014-08-12 00:07:14 -0700

    Add handler and notification

--- discuss.php
+++ discuss.php
@@ -9,7 +9,14 @@
   $_SESSION['user_level'] = 5;
   $_SESSION['HTTP_USER_AGENT'] = md5($_SERVER['HTTP_USER_AGENT']);*/
   $extra_style = "<link rel=\"stylesheet\" href=\"css/discuss.css\" />";
+	$extra_js = "<script src=\"js/database.js\"></script>";
   get_header();
+
+  if($_SESSION['user_id'] != NULL){
+    $unread_count = $notification->count_unread($_SESSION['user_id']);
+    $notification_data = $notification->get_notifications($_SESSION['user_id']);
+  }	
+	
   if (empty($_GET['view'])){
     $view = '';
   }
@@ -19,6 +26,58 @@
   
 ?>
 <section id="content">
+  <div id="notifications">
+    <div class="notification-arrow-up"></div>
+    <div id="notification-body">
+      <div id="notification-header">
+        <b>Notifications:</b>
+          <a href="javascript:;" style="float: right; margin-right: 2vw;" onClick="mark_all_read(<?php echo $_SESSION['user_id']; ?>)">Mark all read</a>
+      </div>
+      <?php
+      if(count($notification_data) == 0){
+      ?>
+      <a href="javascript:;">
+      <div id="notification-0" class="notification read">
+        <div class="notification-color" style="background-color: #ccc"></div>
+        <div class="notification-text">No notifications</div>
+      </div>
+      </a>
+      <?php
+      }
+      else{
+        foreach($notification_data as $notif){
+          $notif_data = $notification->get_notif_obj($notif['notification_type'], $notif['item_id']);
+      ?>
+      <a href="<?php echo $notif_data['url']; ?>" onClick="mark_read(<?php echo $notif['id']; ?>)">
+        <div id="notification-<?php echo $notif['id']; ?>" class="notification <?php if($notif['read'] == 0){ echo 'unread'; }else{ echo 'read'; } ?> ">
+          <div class="notification-color" style="background-color: #<?php echo $notif_data['data']['color']; ?>"><?php echo substr($notif_data['data']['location'], 0, 1); ?></div>
+          <div class="notification-text">
+            <?php echo $notif_data['data']['subject']; ?>
+          </div>
+          <p class="time">
+             <?php echo date('D M d, Y g:i a', $notif['timestamp']); ?>
+          </p>
+        </div>
+      </a>
+      <?php
+        }
+      }
+      ?>
+      <div id="notification-footer">
+        <a href="notifications.php">See All</a>
+      </div>
+    </div>
+  </div>
+        <div id="navigation">
+          <nav class="discuss-nav">
+            <ul>
+              <li><a href="/" id="nav-home">EvDo Home</a></li>
+            <?php if(isset($_SESSION['user_id'])) { ?>
+              <li><a href="javascript:;" class="notification-link" onClick="show_notifications()">Notifications (<?php echo $unread_count; ?>)</a></li>
+            <?php } ?>
+            </ul>
+          </nav>
+        </div>
   <?php if (!empty($_SESSION['user_id'])){ ?>
   <h3>Welcome, <?php echo get_user($_SESSION['user_id']);?>!</h3>
   <br/>
@@ -105,9 +164,6 @@
         break;
       case 'topic':
         include('include/discuss/topic_body.php');
-        break;
-      case 'create':
-        include('include/discuss/create_body.php');
         break;
       default:
         echo "<b>Something wrong happened!</b> Discuss can't handle this request because it doesn't know how to do it! Don't worry, though; Try going <a href='discuss.php'>back to Discuss home page</a> or try our other services!";
