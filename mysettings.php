2d73f0570cc19ba3cbc147906a5dd766d424314f
tyxchen <tyxchen@outlook.com>
2014-08-16 15:05:54 -0400

    Add notifications to account pages
    AdminCP, My Account, My Settings

--- mysettings.php
+++ mysettings.php
@@ -53,11 +53,60 @@ $rs_settings->execute(array($_SESSION['user_id']));
 ?>
 <?php
   $title = "My Settings";
+  $extra_js = "<script src=\"js/index.js\"></script>";
   //dbc already included
   page_protect();
-  get_header();
+
+  if($_SESSION['user_id'] != NULL) {
+    $unread_count = $notification->count_unread($_SESSION['user_id']);
+    $notification_data = $notification->get_notifications($_SESSION['user_id']);
+  }
+
+  get_header(0, $unread_count);
 ?>
 <section id="content">
+  <div id="notifications">
+    <div class="notification-arrow-up"></div>
+    <div id="notification-body">
+      <div id="notification-header">
+        <b>Notifications:</b>
+        <a href="javascript:;" style="float: right; margin-right: 2vw;" onClick="mark_all_read(<?php echo $_SESSION['user_id']; ?>)">Mark all read</a>
+      </div>
+      <?php
+      if(count($notification_data) == 0) {
+      ?>
+      <a href="javascript:;">
+      <div id="notification-0" class="notification read">
+        <div class="notification-color" style="background-color: #ccc"></div>
+        <div class="notification-text">No notifications</div>
+      </div>
+      </a>
+      <?php
+      } else {
+        foreach($notification_data as $notif) {
+          $notif_data = $notification->get_notif_obj($notif['notification_type'], $notif['item_id']);
+      ?>
+      <a href="<?php echo $notif_data['url']; ?>" class="notification-item-link" onClick="mark_read(<?php echo $notif['id']; ?>)">
+        <div id="notification-<?php echo $notif['id']; ?>" class="notification <?php if($notif['read'] == 0){ echo 'unread'; }else{ echo 'read'; } ?> ">
+          <div class="notification-color" style="background-color: #<?php echo $notif_data['data']['color']; ?>"><?php echo substr($notif_data['data']['location'], 0, 1); ?></div>
+          <div class="notification-text">
+            <?php echo $notif_data['data']['subject']; ?>
+          </div>
+          <p class="time">
+             <?php echo date('D M d, Y g:i a', $notif['timestamp']); ?>
+          </p>
+        </div>
+      </a>
+      <?php
+        }
+      }
+      ?>
+      <div id="notification-footer">
+        <a href="notifications.php">See All</a>
+      </div>
+    </div>
+  </div>
+
   <?php
   if (!empty($err)) {
     echo "<p id=\"errors\">";
