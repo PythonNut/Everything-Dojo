767874239b5e59698888adbf7477ce32d8870cb0
DG Kim <donggyunkim@gmail.com>
2014-08-26 11:10:54 -0700

    Fix conflict

--- database.php
+++ database.php
@@ -43,13 +43,8 @@
         foreach($notification_data as $notif){
           $notif_data = $notification->get_notif_obj($notif['notification_type'], $notif['item_id']);
       ?>
-<<<<<<< HEAD
-      <a href="<?php echo $notif_data['url']; ?>" class="notification-item-link">
-        <div id="notification-<?php echo $notif['id']; ?>" class="notification <?php if($notif['read'] == 0){ echo 'unread'; }else{ echo 'read'; } ?> " onClick="mark_read(<?php echo $notif['id']; ?>)">
-=======
       <a href="<?php echo $notif_data['url']; ?>" class="notification-item-link" onClick="mark_read(<?php echo $notif['id']; ?>)">
         <div id="notification-<?php echo $notif['id']; ?>" class="notification-item <?php if($notif['read'] == 0){ echo 'unread'; }else{ echo 'read'; } ?> ">
->>>>>>> FETCH_HEAD
           <div class="notification-color" style="background-color: #<?php echo $notif_data['data']['color']; ?>"><?php echo substr($notif_data['data']['location'], 0, 1); ?></div>
           <div class="notification-text">
             <?php echo $notif_data['data']['subject']; ?>
