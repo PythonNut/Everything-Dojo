46d171937d79e038026c6fe00890c275fe9183b0
Tungsten314 <tungsten314@gmail.com>
2014-08-28 15:01:19 -0400

    Search unfocuses on enter and when the search icon thing is clicked.

--- js/db-search.js
+++ js/db-search.js
@@ -14,22 +14,63 @@ function containsAny(needle, haystack) {
   return false;
 }
 
+// Override the default string trimming methods to be more useful
+String.prototype.trimLeft = function(charlist) {
+  if (charlist === undefined) {
+    charlist = "\\s";
+  }
+
+  return this.replace(new RegExp("^[" + charlist + "]+"), "");
+};
+
+String.prototype.trimLeft = function(charlist) {
+  if (charlist === undefined) {
+    charlist = "\\s";
+  }
+
+  return this.replace(new RegExp("[" + charlist + "]+$"), "");
+};
+
+String.prototype.trim = function(charlist) {
+  return this.trimLeft(charlist).trimRight(charlist);
+};
+
 $(document).ready(function () {
   $(".search").on("propertychange keyup input paste", function () {
     $(".style").each(function () {
       var mainText = $(this).children().first().text().toLowerCase();
-      var author = $(this).children().first().next().text().toLowerCase();
-      var version = $(this).children().last().prev().text().toLowerCase();
       var stage = $(this).children().last().text().toLowerCase();
 
       var query = $(".search").val().toLowerCase();
 
-      if (! (containsAny(query, mainText))) {
+      var author = $(this).children().first().next().text().toLowerCase();
+      var qAuthor = query.match(/@[a-zA-Z0-9_]*/i);
+
+      if (qAuthor !== null) {
+        qAuthor = qAuthor[0].trimLeft("@"); // JS doesn't have lookbehind so the @ needs to be trimmed.
+      }
+
+      else {
+        qAuthor = "";
+      }
+
+      if (! (containsAny(query, mainText) && contains(qAuthor, author))) {
         $(this).fadeOut();
       }
+
       else {
         $(this).fadeIn();
       }
     });
+  });
+
+  $(".search").keypress(function (key) {
+    if (key.which == 13) {
+      $(this).blur();
+    }
+  });
+
+  $(".search-box .icon-box").click(function () {
+    $(".search").blur();
   });
 });
