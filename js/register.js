7cb6dec95bc29b806e41b7d335e23a66eaab4f58
tyxchen <tyxchen@outlook.com>
2014-09-02 14:39:15 -0400

    Change registration validation to use Message()
    Also improved Message()

--- js/register.js
+++ js/register.js
@@ -1,205 +1,149 @@
-/*********************************
- * Verification for register.php *
- *********************************/
-
-// Create div to wrap messages in
-// Placed here so as to not mess them up
-var div = document.createElement("div");
+/* jshint browser:true */
+/* global Message:false */
 
 /**
- * Adds messages next to elements
- * @param {String} selector Valid CSS selector to select element to add message next to
- * @param {String} [msg="removeAll"] Message to display to user.
- * @param {String} msgClass Type of message. Can be "error", "valid", or "notification".
- * @param {Function} fn Function that can be called on remove or other
+ * Provides field validation to register.php
  */
 
-function message(selector, msg, msgClass, fn)
-{
-  // Default argument testing
-  msg = msg || "removeAll";
+document.onready = function () {
 
-  // Correllate msgClass with corresponding class for selector
-  var type = {
-    "error"       : " invalid",
-    "valid"       : " good",
-    "notification": ""
-  };
+  "use strict";
 
-  // Get elClass
-  var elClass  = type[msgClass];
+  /**
+   * Set variables
+   */
 
-  // Get element to display message next to
-  var el = document.querySelector(selector);
+  // Messages
+  var userName   = new Message("[name='user_name']"),
+      userEmail  = new Message("[name='usr_email']"),
+      userPwd    = new Message("[name='pwd']"),
+      userPwdVal = new Message("[name='pwd2']");
 
-  if (msg != "remove" && msg != "removeAll") {
-    el.className += (el.className.indexOf(elClass) == -1) ? elClass : "";
+  // AJAX stuff
+  var ajaxName  = new XMLHttpRequest(),
+      ajaxEmail = new XMLHttpRequest();
 
-    // insert message in DOM right after element
-    el.parentNode.insertBefore(div, el.nextSibling);
-    div.className = msgClass;
-    div.innerHTML = msg;
-  } else if (el.nextElementSibling && el.nextElementSibling.className == msgClass) {
-    // removes error if msg is removeAll
-    if (msg == "removeAll") {
-      el.className = el.className.replace(elClass, "");
-    }
-    el.parentNode.removeChild(el.nextElementSibling);
-  }
-
-  if (typeof fn === "function") {
-    fn();
-  }
-}
-
-/**
- * Throws verification error(s). Is a wrapper for message().
- * @param {String} name Name of element with error
- * @param {String} [msg="removeAll"] Error message to display to user. Takes same arguments as `msg` in message()
- */
-
-function err(name, msg)
-{
-  // Get field with error
-  var element = "[name='" + name + "']";
-
-  message(element, msg, "error", function() {
-    var submit = document.getElementById("doRegister");
-    (msg != "remove" && msg != "removeAll") ? submit.setAttribute("disabled", "disabled") : submit.removeAttribute("disabled");
-  });
-}
-
-/**
- * Tells user that a field is valid. Is a wrapper for message().
- * @param {String} name Name of valid field
- * @param {String} [msg="removeAll"] Message to display to user. Takes same arguments as `msg` in message()
- */
-
-function valid(name, msg)
-{
-  // Get field
-  var element = "[name='" + name + "']";
-
-  message(element, msg, "valid");
-}
-
-/**
- * Validates text that is entered into the fields.
- * @param {String} name Name of field to be validated
- */
-
-function validate(name)
-{
-  // Get element
-  var field = document.querySelector("[name='" + name + "']");
+  /**
+   * Validation
+   */
 
   // Username
-  // window.userTimeout is an ugly global variable hack so that we can
-  // preserve the UX delay without any bugs caused by it (e.g. invalid
-  // username message being overwritten by valid username message)
-  if (name == "user_name") {
-    var user = field.value;
-    if (!user.match(/^[a-z\d_]{3,20}$/i)) {
-      clearTimeout(window.userTimeout);
-      valid(name);
-      err(name, "Invalid username. Usernames must be 3-20 characters long and can only contain alphanumeric characters and underscores.");
-      field.parentNode.lastElementChild.style.display = "none"; // hide loading gif
-    } else {
-      clearTimeout(window.userTimeout);
-      err(name);
-      valid(name);
-      field.parentNode.lastElementChild.style.display = "inline"; // show loading gif
-      // ajax to verify username
-      var ajaxName = new XMLHttpRequest();
-      ajaxName.onreadystatechange = function() {
-        if (ajaxName.readyState == 4 ) {
-          if (ajaxName.status == 200) {
-            err(name);
-            valid(name, "Username is valid.");
-          }
-          else if (ajaxName.status == 400) {
-            valid(name);
-            err(name, "Username already exists! Please choose a new one.");
-          }
-          else {
-            valid(name);
-            err(name, "A " + ajaxName.status + " error occurred. Please try again.", "error");
-          }
-          field.parentNode.lastElementChild.style.display = "none"; // hide loading gif
-        }
-      };
+  userName.el.onkeyup = function (e) {
+    var user = userName.el.value,
+        keycode = ('which' in e) ? e.which : e.keyCode; // key validation
 
-      // set 400ms timeout because we're getting results too fast, and
-      // users won't know whether the AJAX got through or not :P
-      window.userTimeout = setTimeout(function() {
-        ajaxName.open("GET", "register.php?username=" + user, true);
-        ajaxName.send();
-      }, 400);
+    if ((keycode > 47 && keycode < 58) || (keycode > 64 && keycode < 91) || (keycode > 95 && keycode < 112) || keycode == 173 || keycode == 8 || keycode == 46) {
+      if (!user.match(/^[a-z\d_]{3,20}$/i)) {
+        clearTimeout(window.userTimeout);
+        ajaxName.abort(); // abort ajax request if already sent
+        userName.assign("Invalid username. Usernames must be 3-20 characters long and can only contain alphanumeric characters and underscores.", "error").show();
+        userName.el.parentNode.lastElementChild.style.display = "none"; // hide loading gif
+      } else {
+        clearTimeout(window.userTimeout);
+        ajaxEmail.abort();
+        userName.purge();
+        userName.el.parentNode.lastElementChild.style.display = "inline"; // show loading gif
+        // ajax to verify username
+        ajaxName.onreadystatechange = function() {
+          if (ajaxName.readyState == 4 ) {
+            if (ajaxName.status == 200) {
+              userName.assign("Username is valid.", "correct").show();
+            } else if (ajaxName.status == 400) {
+              userName.assign("Username already exists! Please choose a new one.", "error").show();
+            } else {
+              userName.assign("A " + ajaxName.status + " error occurred. Please try again.", "error").show();
+            }
+            userName.el.parentNode.lastElementChild.style.display = "none"; // hide loading gif
+          }
+        };
+
+        // set 400ms timeout because we're getting results too fast, and
+        // users won't know whether the AJAX got through or not :P
+        window.userTimeout = setTimeout(function() {
+          ajaxName.open("GET", "register.php?username=" + user, true);
+          ajaxName.send();
+        }, 300);
+      }
     }
-  }
+  };
 
   // Email
-  // For explanation of window.emailTimeout, see Username.
-  if (name == "usr_email") {
-    var email = field.value;
-    if (!email.match(/^\S+@(localhost|([\w\d-]{2,}\.){1,2}[\w]{2,6})$/i)) {
-      clearTimeout(window.emailTimeout);
-      valid(name);
-      err(name, "Email entered is not a valid email.");
-      field.parentNode.lastElementChild.style.display = "none"; // hide loading gif
-    } else {
-      clearTimeout(window.emailTimeout);
-      err(name);
-      valid(name);
-      field.parentNode.lastElementChild.style.display = "inline"; // show loading gif
-      // ajax to verify email
-      var ajaxEmail = new XMLHttpRequest();
-      ajaxEmail.onreadystatechange = function() {
-        if (ajaxEmail.readyState == 4 ) {
-          if (ajaxEmail.status == 200) {
-            err(name);
-            valid(name, "Email is valid.");
-          }
-          else if (ajaxEmail.status == 400) {
-            valid(name);
-            err(name, "Email address already exists in our database! Please do not create multis.");
-          }
-          else {
-            valid(name);
-            err(name, "A " + ajaxEmail.status + " error occurred. Please try again.", "error");
-          }
-          field.parentNode.lastElementChild.style.display = "none"; // hide loading gif
-        }
-      };
+  userEmail.el.onkeyup = function (e) {
+    var email = userEmail.el.value,
+        keycode = ('which' in e) ? e.which : e.keyCode; // key validation
 
-      // set 400ms timeout for same reasons as with username
-      window.emailTimeout = setTimeout(function() {
-        ajaxEmail.open("GET", "register.php?email=" + email, true);
-        ajaxEmail.send();
-      }, 400);
+    if ((keycode > 47 && keycode < 58) || (keycode > 64 && keycode < 91) || (keycode > 95 && keycode < 112) || keycode == 173 || keycode == 8 || keycode == 46) {
+      if (!email.match(/^\S+@(localhost|([\w\d-]{2,}\.){1,2}[\w]{2,6})$/i)) {
+        clearTimeout(window.emailTimeout);
+        ajaxEmail.abort();
+        userEmail.assign("Email entered is not a valid email.", "error").show();
+        userEmail.el.parentNode.lastElementChild.style.display = "none"; // hide loading gif
+      } else {
+        clearTimeout(window.emailTimeout);
+        ajaxEmail.abort();
+        userEmail.purge();
+        userEmail.el.parentNode.lastElementChild.style.display = "inline"; // show loading gif
+        // ajax to verify email
+        ajaxEmail.onreadystatechange = function() {
+          if (ajaxEmail.readyState == 4 ) {
+            if (ajaxEmail.status == 200) {
+              userEmail.assign("Email is valid.", "correct").show();
+            } else if (ajaxEmail.status == 400) {
+              userEmail.assign("Email address already exists in our database! Please do not create multis.", "error").show();
+            } else {
+              userEmail.assign("A " + ajaxEmail.status + " error occurred. Please try again.", "error").show();
+            }
+            userEmail.el.parentNode.lastElementChild.style.display = "none"; // hide loading gif
+          }
+        };
+
+        // set 400ms timeout for same reasons as with username
+        window.emailTimeout = setTimeout(function() {
+          ajaxEmail.open("GET", "register.php?email=" + email, true);
+          ajaxEmail.send();
+        }, 300);
+      }
     }
-  }
+  };
 
   // Password
-  if (name == "pwd") {
-    var pwd = field.value;
+  userPwd.el.onkeyup = function () {
+    var pwd = userPwd.el.value;
     if (pwd.length < 6) {
-      err(name, "Passwords must be at least 6 characters long.");
+      userPwd.assign("Passwords must be at least 6 characters long.", "error").show();
     } else {
-      err(name);
+      userPwd.purge();
     }
-  }
+  };
 
-  // Password verification
-  // There has to be a better way to do this
-  if (name == "pwd2") {
+  // Check password
+  userPwdVal.el.onkeyup = function () {
     var original = document.querySelector("[name='pwd']").value,
-        verify   = field.value;
+        verify   = userPwdVal.el.value;
     if (original != verify) {
-      err(name, "Passwords do not match.");
+      userPwdVal.assign("Passwords do not match.", "error").show();
     } else {
-      err(name);
+      userPwdVal.purge();
     }
-  }
-}
+  };
 
+  /**
+   * Onblur effects
+   */
+  userName.el.onblur = function () {
+    userName.hide();
+  };
+
+  userEmail.el.onblur = function () {
+    userEmail.hide();
+  };
+
+  userPwd.el.onblur = function () {
+    userPwd.hide();
+  };
+
+  userPwdVal.el.onblur = function () {
+    userPwdVal.hide();
+  };
+
+};
