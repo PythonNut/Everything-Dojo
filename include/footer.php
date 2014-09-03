c0fb4cbdecb420ba90794871f76e7aa44eabbced
Tungsten314 <tungsten314@gmail.com>
2014-09-03 18:19:30 -0400

    Made unicorns (mostly) sticky.

--- include/footer.php
+++ include/footer.php
@@ -36,6 +36,51 @@
       <p>Server Time: <?php echo date("m/d/y G:i T"); ?></p>
     </footer>
 
+    <!-- Unicorns should be sticky -->
+    <script class="js-unicorns">
+      // Function to get search parameters as an associative array. Written by StackOverflow user weltraumpirat at http://stackoverflow.com/questions/5448545/how-to-retrieve-get-parameters-from-javascript/5448635#5448635
+      function getSearchParameters() {
+            var prmstr = window.location.search.substr(1);
+            return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+      }
+
+      function transformToAssocArray( prmstr ) {
+          var params = {};
+          var prmarr = prmstr.split("&");
+          for ( var i = 0; i < prmarr.length; i++) {
+              var tmparr = prmarr[i].split("=");
+              params[tmparr[0]] = tmparr[1];
+          }
+          return params;
+      }
+
+      var params = getSearchParameters();
+
+      if (params["unicorns"] == "true") {
+        // Code for finding all internal links by Chris Coyier at http://css-tricks.com/snippets/jquery/find-all-internal-links/
+        var siteURL = "http://" + top.location.host.toString();
+
+        $("a[href^='"+siteURL+"'], a[href^='/'], a[href^='./'], a[href^='../'], a[href^='#']").each(function() {
+          var href = $(this).attr("href");
+
+          if (href.indexOf(".php") > -1) { // Find php files
+            $(this).attr("href", href + (href.indexOf("?") > -1 ? "&unicorns=true" : "?unicorns=true"));
+          }
+
+          if (href == "rebuild.everythingdojo.com" || href == "rebuild.everythingdojo.com/") { // Links to the home page don't have .php in their URL
+            $(this).attr("href", "rebuild.everythingdojo.com?unicorns=true");
+          }
+
+          if (href == "/") {
+            $(this).attr("href", "/?unicorns=true");
+          }
+        });
+
+        // FIXME this doesn't work for the tools links
+        $(document).ready(function () {$(".index-link").attr("href", $(".index-link").attr("href") + "?unicorns=true");}); // For some reason, the links to the various bits of the site need to wait until the DOM is loaded
+      }
+    </script>
+
   </body>
 
 </html>
