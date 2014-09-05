14ede62b6034b31b941a257b6bcc00550b3555cc
tyxchen <tyxchen@outlook.com>
2014-09-05 17:47:21 -0400

    Discuss styling, part one.
    Add Cancel button, make textareas full width

--- include/discuss/forum_body.php
+++ include/discuss/forum_body.php
@@ -5,7 +5,7 @@
   }
   else if($id == ''){
     redirect(URL_DISCUSS);
-  } 
+  }
   else{
     if(!isset($_SESSION['user_id'])){
       $user_id = 0;
@@ -15,7 +15,7 @@
     }
     $topics = $discuss->get_topics(intval($id), $user_id);
   }
-  
+
   if ($id == 1){
     $typearg = 1;
   }
@@ -29,18 +29,18 @@
 <section id="discuss-topics" style="clear:left;">
   <h3 style="text-align: center;"><?php echo $forum_data['name']; ?></h3>
   <p style="text-align: center;"><?php echo $forum_data['description']; ?></p>
-  <?php 
+  <?php
   if ($user_id > 0 and intval($id) != 1){
     echo "<a href=\"#topic-create-topic\" id=\"topic-a-topic\" style=\"left: 5%; position: relative;\">+ Create New Topic</a><br />";
-		if(!empty($_SESSION['err'])){
-			echo '<div id="errors">';
-			foreach($_SESSION['err'] as $error){
-				echo '<p class="invalid">' . $error . '</p><br />';
-			}
-			echo '</div>';
-		}
-		unset($_SESSION['err']);
-	?>  
+    if(!empty($_SESSION['err'])){
+      echo '<div id="errors">';
+      foreach($_SESSION['err'] as $error){
+        echo '<p class="invalid">' . $error . '</p><br />';
+      }
+      echo '</div>';
+    }
+    unset($_SESSION['err']);
+  ?>
   <fieldset id="topic-create-topic">
   <legend>Add new topic</legend>
   <form action="include/discuss-handler.php" method="post">
@@ -48,24 +48,29 @@
     Message: <br /><textarea name="desc" placeholder="Write your post here..." style="vertical-align:top; height:200px;"></textarea><br/>
     <input type="hidden" name="forum" value="<?php echo $id;?>" />
     <input type="hidden" name="mode" value="topic">
-    <input type="submit" value="Post"/>
+    <input type="button" value="Cancel" class="danger" id="cancel" />
+    <input type="submit" value="Post" id="post" />
   </form>
   </fieldset>
-  
+
   <script>
     $("#topic-create-topic").hide();
     $("#topic-a-topic").click(function(){
       $("#topic-a-topic").hide();
       $("#topic-create-topic").slideToggle(300);
     });
+    $("#cancel").click(function () {
+      $("#topic-create-topic").slideToggle(300);
+      $("#topic-a-topic").show();
+    });
   </script>
   <?php
   }
-	if($_SESSION['user_id'] != 0){
-	?>
+  if($_SESSION['user_id'] != 0){
+  ?>
   <a href="javascript:;" onClick="mark_all_read(<?php echo $id . ', ' . $_SESSION['user_id']; ?>)" style="left: 5%; position: relative;">Mark All Read</a>
   <?php
-	}
+  }
   ?>
     <table class="discuss-table">
       <thead style="border-bottom: 1px black solid;">
@@ -74,49 +79,49 @@
           <td class="med-col center">Author</td>
           <td class="small-col center">Comments</td>
           <td class="small-col center">Viewed By</td>
-          <td class="med-col center">Last Post</td> 
+          <td class="med-col center">Last Post</td>
         <tr>
       </thead>
       <tbody>
       <?php if(count($topics) == 0){ ?>
-      	<tr style="cursor:pointer;">
+        <tr style="cursor:pointer;">
           <td colspan="2">No topics</td>
           <td class="center">-</td>
           <td class="center">-</td>
           <td class="center">-</td>
           <td class="center">-</td>
         </tr>
-      <?php } else{ 
-		foreach($topics as $topic){ 
-		  $username = get_user($topic['user_id']);
-		  $comments = $discuss->get_comment_count($topic['topic_id'], $type);
-		  if($type == 1){
-		    $comments = $comments - 1;
-		  }
+      <?php } else{
+    foreach($topics as $topic){
+      $username = get_user($topic['user_id']);
+      $comments = $discuss->get_comment_count($topic['topic_id'], $type);
+      if($type == 1){
+        $comments = $comments - 1;
+      }
       ?>
         <tr style="cursor:pointer;" onclick="window.location.href='<?php echo URL_DISCUSS; ?>?view=topic&f=<?php echo intval($id); ?>&t=<?php echo $topic['topic_id']; ?>'">
-					<td class="tiny-col"><p class="topic-icon <?php if($topic['read'] == 1){ echo 'read-icon'; }else{ echo 'unread-icon'; }?>"></p></td>
+          <td class="tiny-col"><p class="topic-icon <?php if($topic['read'] == 1){ echo 'read-icon'; }else{ echo 'unread-icon'; }?>"></p></td>
           <td><?php echo htmlspecialchars($topic['title']); ?></td>
           <td class="center"><?php echo $username; ?></td>
           <td class="center"><?php echo $comments; ?></td>
           <td class="center"><?php echo $discuss->get_views($topic['topic_id'], 1 - $typearg); ?></td>
-          <td class="center"><?php $lastpost = $discuss->get_posts(intval($topic['topic_id']), 'all', $typearg); 
-					if (empty($lastpost)){ 
-						echo date('M d, Y g:i a', $topic['time'])."<br /><b>".get_user($topic['user_id'])."</b>";
-					} 
-					else{ 
-						echo date('M d, Y g:i a', $lastpost[count($lastpost)-1]['time'])."<br /><b>".get_user($lastpost[count($lastpost)-1]['user_id'])."</b> "; 
-					} ?></td>
+          <td class="center"><?php $lastpost = $discuss->get_posts(intval($topic['topic_id']), 'all', $typearg);
+          if (empty($lastpost)){
+            echo date('M d, Y g:i a', $topic['time'])."<br /><b>".get_user($topic['user_id'])."</b>";
+          }
+          else{
+            echo date('M d, Y g:i a', $lastpost[count($lastpost)-1]['time'])."<br /><b>".get_user($lastpost[count($lastpost)-1]['user_id'])."</b> ";
+          } ?></td>
         </tr>
       <?php }
-			} ?>
+      } ?>
       </tbody>
     </table>
     <?php
-			if($_SESSION['user_id'] != 0){
-		?>
-		<a href="javascript:;" onClick="mark_all_read(<?php echo $id . ', ' . $_SESSION['user_id']; ?>)" style="left: 5%; position: relative;">Mark All Read</a>
-		<?php
-		}
-		?>
+      if($_SESSION['user_id'] != 0){
+    ?>
+    <a href="javascript:;" onClick="mark_all_read(<?php echo $id . ', ' . $_SESSION['user_id']; ?>)" style="left: 5%; position: relative;">Mark All Read</a>
+    <?php
+    }
+    ?>
 </section>
