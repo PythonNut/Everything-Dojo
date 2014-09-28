eec5a2a079e2b4f72edccde411f8fba02a3f486f
tyxchen <tyxchen@outlook.com>
2014-09-27 21:58:48 -0400

    Minify CSS/JS files

--- database.php
+++ database.php
@@ -5,7 +5,7 @@
   session_start();
   $extra_style = "<link rel=\"stylesheet\" href=\"css/prism.css\" />
   <link rel=\"stylesheet\" href=\"css/database.css\" />";
-  $extra_js = "<script src=\"js/prism.js\"></script>
+  $extra_js = "<script src=\"js/prism.min.js\"></script>
   <script src=\"js/database.js\"></script>";
   get_header();
 
